59b7f2ff662cf269e63320d15f8749d1d2d7ed04
Vinícius Lima <villylss@gmail.com>
2022-09-14 10:51:05 -0300

    feature: calculo de tempo adicionado

--- lab2/atividade1.c
+++ lab2/atividade1.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "timer.h"
 
 int main(int argc, char*argv[]) {
-   float *matriz1, *matriz2; //matrizes que serão carregadas do arquivo
+   float *matriz1, *matriz2, *matrizResultado;
    int linhas1, colunas1, linhas2, colunas2; //dimensoes das matrizes
    long long int tam1, tam2, tamResultante; //qtde de elementos nas matrizes
    FILE * descritorArquivoEntrada1, * descritorArquivoEntrada2, * descritorArquivoSaida; //descritores dos arquivos
    size_t ret1, ret2, retSaida; //retorno da funcao de leitura nos arquivos 
+   double inicio, fim, delta;
+   float aux = 0; 
 
+   GET_TIME(inicio);
    //recebe os argumentos de entrada
    if(argc < 4) {
       fprintf(stderr, "Digite: %s <arquivo entrada1> <arquivo entrada2> <arquivo saida>\n", argv[0]);
@@ -51,7 +55,8 @@ int main(int argc, char*argv[]) {
    //aloca memoria para as matrizes
    matriz1 = (float*) malloc(sizeof(float) * tam1);
    matriz2 = (float*) malloc(sizeof(float) * tam2);
-   if(!matriz1 || !matriz2) {
+   matrizResultado = (float*) malloc(sizeof(float) * tamResultante);
+   if(!matriz1 || !matriz2 || !matrizResultado) {
       fprintf(stderr, "Erro de alocao da memoria da matriz\n");
       return 3;
    }
@@ -64,65 +69,62 @@ int main(int argc, char*argv[]) {
       return 4;
    }
 
-   //imprime a matriz na saida padrao
-   for(int i=0; i<linhas1; i++) { 
-      for(int j=0; j<colunas1; j++)
-        fprintf(stdout, "%.6f ", matriz1[i*colunas1+j]);
-      fprintf(stdout, "\n");
-   }
-
-   printf("\n\n\n\n\n");
-
-   for(int i=0; i<linhas2; i++) { 
-      for(int j=0; j<colunas2; j++)
-        fprintf(stdout, "%.6f ", matriz2[i*colunas2+j]);
-      fprintf(stdout, "\n");
-   }
-
-   printf("\n\n\n\n\n");
+   GET_TIME(fim);
+   delta = fim - inicio;
+   printf("Tempo inicialização: %lf\n", delta);
 
    // realiza o produto das matrizes
-   float matrizResultado[linhas1][colunas2], aux = 0; 
+   GET_TIME(inicio);
 
    if(colunas1 == linhas2) {
       for(int i = 0; i < linhas1; i++){
-         for(int j = 0; j < colunas2; j++){
-
-            matrizResultado[i][j] = 0;
+        for(int j = 0; j < colunas2; j++){
+         matrizResultado[i*linhas1+j] = 0;
             for(int z = 0; z < linhas2; z++){
                aux += matriz1[i*linhas1+z] * matriz2[z*linhas2+j];
             }
 
-            matrizResultado[i][j] = aux;
-            aux = 0;
-         }
+         matrizResultado[i*linhas1+j] = aux;
+         aux = 0;
+        }
       }
    } else {
       fprintf(stderr, "Não é possível multiplicar as matrizes dadas\n");
       return 5;
    }
 
+   GET_TIME(fim);
+   delta = fim - inicio;
+   printf("Tempo multiplicação: %lf\n", delta);
+
    // imprime matriz resultante
-   for(int i=0; i<linhas1; i++) { 
-      for(int j=0; j<colunas2; j++)
-        fprintf(stdout, "%.6f ", matrizResultado[i][j]);
-      fprintf(stdout, "\n");
-   }
+   // for(int i=0; i<linhas1; i++) { 
+   //    for(int j=0; j<colunas2; j++)
+   //      fprintf(stdout, "%.6f ", matrizResultado[i*linhas1+j]);
+   //    fprintf(stdout, "\n");
+   // }
+
+   // Finalização 
+   GET_TIME(inicio);
 
    //abre o arquivo para escrita binaria
+
    descritorArquivoSaida = fopen(argv[3], "wb");
    if(!descritorArquivoSaida) {
       fprintf(stderr, "Erro de abertura do arquivo\n");
       return 3;
    }
 
-   // escreve a matriz
+    //escreve numero de linhas e de colunas
+   retSaida = fwrite(&linhas1, sizeof(int), 1, descritorArquivoSaida);
+   retSaida = fwrite(&colunas2, sizeof(int), 1, descritorArquivoSaida);
+
+   // escreve os elementos da matriz
    retSaida = fwrite(matrizResultado, sizeof(float), tamResultante, descritorArquivoSaida);
    if(retSaida < tamResultante) {
       fprintf(stderr, "Erro de escrita no arquivo\n");
       return 4;
    }
-
 
    //finaliza o uso das variaveis
    fclose(descritorArquivoEntrada1);
@@ -130,5 +132,11 @@ int main(int argc, char*argv[]) {
    fclose(descritorArquivoSaida);
    free(matriz1);
    free(matriz2);
+   free(matrizResultado);
+
+   GET_TIME(fim);
+   delta = fim - inicio;
+   printf("Tempo finalização: %lf\n", delta);
+
    return 0;
 }
