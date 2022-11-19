f8453666436770690609815fde4fb81c9bb843f0
Vinícius Lima <villylss@gmail.com>
2022-11-19 17:21:45 -0300

    feature: ordenacao concorrente adicionada

---
+++ trabalho-final/ordenacaoConcorrente.c
@@ -0,0 +1,219 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include "timer.h"
+
+int * vetor;
+int numThreads; // número de threads
+long long int tam; // tamanho do vetor 
+int aux = 1; // variável auxiliar para verificar subdivisão do vetor 
+
+
+// função para impressão do vetor 
+void imprimeVetor(int vet[], int tam){
+    for(int i = 0; i < tam; i++){
+        printf(" %d ", vet[i]); 
+    }
+    printf("\n");
+}
+
+
+// parte do merge sort 
+void merge(int vet[], int inicio, int meio, int fim){
+    //criando um vetor auxiliar e copiando o vetor original
+    int *tmp;
+    tmp = (int*)malloc(sizeof(int) * (fim - inicio));
+
+    int i = inicio;
+    int j = meio;
+    int pos = 0;
+
+    while(i < meio && j < fim){
+        if(vet[i] <= vet[j]){
+            tmp[pos++] = vet[i++];
+        } else{
+            tmp[pos++] = vet[j++];
+        }
+    }
+
+    while(i < meio){
+        tmp[pos++] = vet[i++];
+    }
+
+    while(j < fim){
+        tmp[pos++] = vet[j++];
+    }
+
+    for(i = inicio; i < fim; i++){
+        vet[i] = tmp[i - inicio];
+    }
+
+    free(tmp);
+}
+
+// algoritmo merge sort 
+void mergeSort(int vet[], int inicio, int fim){
+    if(inicio < (fim - 1)){
+        int meio = (inicio + fim)/ 2;
+        mergeSort(vet, inicio, meio);
+        mergeSort(vet, meio, fim);
+        merge(vet, inicio, meio, fim);
+    }
+}
+
+
+// parte do quick sort
+int particiona(int vet[], int inicio, int fim){
+    //pivot randomico
+    int p = inicio + rand()%(fim - inicio + 1);
+    troca(&vet[inicio], &vet[p]);
+    
+    int pivot = vet[inicio];
+    int i = inicio;
+    int j = fim ;
+    
+    while(i < j){
+        while(i < fim && vet[i] <= pivot){
+            i++;
+        }
+        while(j > inicio && vet[j] >= pivot){
+            j--;
+        }
+        if(i < j){
+            troca(&vet[i], &vet[j]);
+        }
+    }
+    vet[inicio] = vet[j];
+    vet[j] = pivot;
+
+    return j; 
+}
+
+// algoritmo quick sort 
+void quick_sort(int vet[], int inicio, int fim){
+    if(inicio < fim){
+        int pivot = particiona(vet, inicio, fim);
+        quick_sort(vet, inicio, pivot - 1);
+        quick_sort(vet, pivot + 1, fim);
+    }
+}
+
+
+// fluxo das threads
+void * ordena (void * arg) {
+
+    free(arg);
+    pthread_exit(NULL);
+}
+
+
+// fluxo principal
+int main (int argc, char * argv[]) { 
+    FILE * descritorArquivoEntrada, * descritorArquivoSaida; //descritores dos arquivos
+    size_t retEntrada, retSaida; //retorno da funcao de leitura nos arquivos 
+    double inicio, fim, delta; // gerenciadores de tempo 
+
+    pthread_t *tid; // identificadores das threads 
+
+    // -- INICIALIZAÇÃO -- //
+
+    //recebe os argumentos de entrada
+    if(argc < 4) {
+        fprintf(stderr, "Digite: %s <arquivo vetor entrada> <arquivo vetor saida> <numero threads>\n", argv[0]);
+        return 1;
+    }
+
+    // recebe o número de threads
+    numThreads = atoi(argv[3]);
+    printf("%d", numThreads);
+
+    //abre o arquivo para leitura binaria do vetor de entrada
+    descritorArquivoEntrada = fopen(argv[1], "rb");
+    if(!descritorArquivoEntrada) {
+        fprintf(stderr, "Erro de abertura do arquivo\n");
+        return 2;
+    }
+
+    //le o tamanho do vetor de entrada
+    retEntrada = fread(&tam, sizeof(long long int), 1, descritorArquivoEntrada);
+    if(!retEntrada) {
+        fprintf(stderr, "Erro de leitura do tamanho do vetor de entrada\n");
+        return 2;
+    }
+
+    //aloca memoria para o vetor
+    vetor = (int*) malloc(sizeof(int) * tam);
+    if(!vetor) {
+        fprintf(stderr, "Erro de alocao da memoria do vetor de entrada\n");
+        return 2;
+    }
+
+    // carrega o vetor de elementos inteiros do arquivo de entrada
+    retEntrada = fread(vetor, sizeof(int), tam, descritorArquivoEntrada);
+    if(retEntrada < tam) {
+        fprintf(stderr, "Erro de leitura dos elementos do vetor de entrada\n");
+        return 2;
+    }
+
+
+    // -- ORDENAÇÃO -- //
+
+    GET_TIME(inicio);
+
+    // aloca identificadores das threads 
+    tid = (pthread_t *) malloc(sizeof(pthread_t) *numThreads);
+    if(tid == NULL){
+        fprintf(stderr, "ERRO--malloc\n");
+        return 2;
+    }
+
+    // cria threads
+    for(long int i = 0; i < numThreads; i++){
+        if(pthread_create(tid+i, NULL, ordena, (void*) i)){
+            fprintf(stderr, "ERRO--pthread_create\n");
+            return 3;
+        }
+    }
+
+    // aguardar o término das threads
+    for(long int i = 0; i < numThreads; i++){
+        if(pthread_join(*(tid+i), NULL)){
+            fprintf(stderr, "ERRO--pthread_join\n");
+        }
+    }
+
+    GET_TIME(fim);
+    delta = fim - inicio;
+    printf("Tempo de ordenação: %lf\n", delta);
+
+    // imprime vetor ordenado (descomentar se desejar visualizar!)
+    // imprimeVetor(vetor, tam);
+
+    // -- FINALIZAÇÃO -- //
+
+    //abre o arquivo para escrita binaria
+    descritorArquivoSaida = fopen(argv[2], "wb");
+    if(!descritorArquivoSaida) {
+        fprintf(stderr, "Erro de abertura do arquivo de saida\n");
+        return 3;
+    }
+
+    //escreve o tamanho do vetor de saída
+    retSaida = fwrite(&tam, sizeof(long long int), 1, descritorArquivoSaida);
+
+    // escreve os elementos da matriz
+    retSaida = fwrite(vetor, sizeof(int), tam, descritorArquivoSaida);
+    if(retSaida < tam) {
+        fprintf(stderr, "Erro de escrita no arquivo de saida\n");
+        return 3;
+    }
+
+    //finaliza o uso das variaveis
+    fclose(descritorArquivoEntrada);
+    fclose(descritorArquivoSaida);
+    free(vetor);
+    free(tid);
+
+    return 0;
+}
